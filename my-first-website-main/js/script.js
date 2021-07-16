6958ab88e036abfac4245bc78accd0c66b0a47f2
Anidhya Gupta <guptaanidhya2000.fb@gmail.com>
2021-07-16 22:43:44 +0530

    ...

--- my-first-website-main/js/script.js
+++ my-first-website-main/js/script.js
@@ -1,9 +1,13 @@
 function openMenu() {
-    document.getElementById("menubar").style.width="20%";
-    document.getElementById("page-content").style.marginLeft="20%";
+    document.getElementById("menubar").style.width="250px";
+    document.getElementById("page-content").style.marginLeft="250px";
 }
 
 function closeBtn() {
     document.getElementById("menubar").style.width="0";
     document.getElementById("page-content").style.marginLeft="0";
+}
+
+function dropdown1(){
+    document.getElementById("dropdown1").style.height="auto";
 }
